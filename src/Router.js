f0e57ac03a6997cd20fe395d09b80b37a8d23e3d
Daniel Tovesson <daniel.tovesson@me.com>
2018-08-09 19:40:30 +0200

    #5 Fix deprecated TabNavigator

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { TabNavigator } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation'
 import MapScreen from './screens/Map/MapScreen'
 import CompaniesScreen from './screens/Companies/CompaniesScreen'
 import EventsScreen from './screens/Events/EventsScreen'
 import AboutScreen from './screens/About/AboutScreen'
 
-const Router = TabNavigator({
+const Router = createBottomTabNavigator({
   Map: {
     screen: MapScreen
   },
