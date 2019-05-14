ba142cdb8d16df4b35a573f3578cf8cb85681160
Joel Bångdal <dat15jba@student.lu.se>
2019-05-14 18:17:24 +0200

    added some comments

--- src/screens/Countdown/CountdownScreen.js
+++ src/screens/Countdown/CountdownScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Alert, Text } from "react-native";
 import TimerCountdown from "react-native-timer-countdown";
 
 const currentDate = Date.now();
+//Date for arkad, change every year
 const arkadDate = new Date('November 13, 2019 10:00:00 GMT+1:00');
 
 const CountdownScreen = () => (
@@ -11,6 +12,7 @@ const CountdownScreen = () => (
       initialMilliseconds={(Date.parse(arkadDate) - currentDate)}
       onTick={(milliseconds) => console.log("tick", milliseconds)}
       formatMilliseconds={(milliseconds) => {
+        // advanced math making sure every date is right
         const remainingSec = Math.round(milliseconds / 1000);
         const seconds = parseInt((remainingSec % 60).toString(), 10);
         const minutes = parseInt(((remainingSec / 60) % 60).toString(), 10);
@@ -22,6 +24,7 @@ const CountdownScreen = () => (
         let h = hours < 10 ? '0' + hours : hours;
         let d = days < 10 ? '0' + days : days;
         let w = weeks < 10 ? '0' + weeks : weeks;
+        // for some reason hours always is 1 hour ahead of what it should be thats why h-1
         h = h === '00' ? '0:' : h-1 + ':';
         d = d === '00' ? '0:' : d + ':';
         w = w === '00' ? '0:' : w + ':';
