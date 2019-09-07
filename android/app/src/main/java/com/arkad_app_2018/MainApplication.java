a8537e6d29249fda458f89efbfb06b7fca2a31ae
Alexander Mjoberg <alexander.mjoberg@gmail.com>
2019-09-07 14:28:19 +0200

    android working, barely

--- android/app/src/main/java/com/arkad_app_2018/MainApplication.java
+++ android/app/src/main/java/com/arkad_app_2018/MainApplication.java
@@ -7,10 +7,7 @@ import com.crashlytics.android.Crashlytics;
 import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
-import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
-import com.smixx.fabric.FabricPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
