c071ccb07a66501e02a230dd72010ba66d0a0451
Alexander Mjoberg <alexander.mjoberg@gmail.com>
2019-09-07 13:57:41 +0200

    Remaining RN upgrade android changes

--- android/app/src/main/java/com/arkad_app_2018/MainApplication.java
+++ android/app/src/main/java/com/arkad_app_2018/MainApplication.java
@@ -1,19 +1,21 @@
 package com.arkad_app_2018;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
 import com.smixx.fabric.FabricPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.wix.RNCameraKit.RNCameraKitPackage;
 import io.fabric.sdk.android.Fabric;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -26,13 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SvgPackage(),
-            new FabricPackage(),
-            new VectorIconsPackage(),
-            new RNCameraKitPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
     }
 
     @Override
