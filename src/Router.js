9035c33cd4d2b62b8461e014fc81ce46d823d53e
Joel Bångdal <j.bangdal@gmail.com>
2019-10-14 19:28:35 +0200

    removed cameratext and added as a subheader

--- src/Router.js
+++ src/Router.js
@@ -191,7 +191,7 @@ const MainStack = createBottomTabNavigator(
             screen: CameraContainer,
             navigationOptions: () => ({
               ...navigationOptions,
-              title: 'Camera',
+              headerTitle: <SubtitleHeader title="Camera" subtitle="Scan the QR-code on a students flipcard" />
             })
           }
         },
