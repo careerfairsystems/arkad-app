4ecc9513fa5d565914030dfbf5d0938893549f72
Daniel Tovesson <daniel.tovesson@me.com>
2019-10-22 08:19:39 +0200

    Bump Android version

--- src/components/listItems/CompanyListItem.js
+++ src/components/listItems/CompanyListItem.js
@@ -14,8 +14,8 @@ const styles = {
 const { title } = styles
 const CompanyListItem = ({ navigation, company }) => (
   <SelectableListItem navigation={navigation} item={company}>
-    <Text style={title}>{company.name}</Text>
-    <FavoriteButton company={company} color={"#000"} />
+    <Text style={title}>Company</Text>
+    <FavoriteButton company={company} color="#000" />
   </SelectableListItem>
 )
 
